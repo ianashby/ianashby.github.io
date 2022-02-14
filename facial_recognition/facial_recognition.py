125f12d2628b05c1e80f00e7a09416741218f25d
ianashby <ianashbyphotography@gmail.com>
2022-02-13 20:16:20 -0700

    update repository

--- facial_recognition/facial_recognition.py
+++ facial_recognition/facial_recognition.py
@@ -5,11 +5,16 @@ Facial Recognition
 
 import cv2
 from deepface import DeepFace
+from collections import Counter
+import collections
 
-# Use the frontface Haar cascade to scan for face on screen.
+# Use the frontalface Haar cascade to scan for face on screen.
 face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
-cv2.namedWindow("preview")
+cv2.namedWindow("Emotion Analysis")
 video = cv2.VideoCapture(2)
+
+# This list will store all the emotions detected while the video is open.
+emotions = ['happy', 'happy', 'sad', 'neutral', 'sad']
 
 # While the camera is on and opened, the program will run. The window will close on ESC.
 while video.isOpened():
@@ -22,25 +27,38 @@ while video.isOpened():
 
     # scaleFactor rescales the input image, resizing a larger face to a smaller one, and making it detectable by the algorithm.
     # minNeighbors affects the quality of detected faces. 
-    face = face_cascades.detectMultiScale(grayscale, scaleFactor = 1.1, minNeighbors = 5)
+    face_detection = face_cascades.detectMultiScale(grayscale, scaleFactor = 1.4, minNeighbors = 5)
 
     # Set the bounding box to scan the face. Create the box lines that will surround the face.
-    for x,y,w,h in face:
+    for x,y,w,h in face_detection:
         img = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
 
         try:
-            # Analyze and print the current real-time emotion from the face on camera.
+            # Analyze, save, and print the current real-time emotion from the face on camera.
             analyze = DeepFace.analyze(frame, actions=['emotion'])
+            emotions.append(analyze['dominant_emotion'])
             print(analyze['dominant_emotion'])
         except:
             print('No face detected.')
 
 
-    cv2.imshow("preview", frame)
+    cv2.imshow("Emotion Analysis", frame)
     key = cv2.waitKey(1)
     # Program will close on ESC.
     if key == 27:
         break
 
 video.release()
-cv2.destroyWindow("preview")
+cv2.destroyWindow("Emotion Analysis")
+
+# Create a dictionary to store how many times each emotion was displayed.
+counter = collections.Counter(emotions)
+total_sum = len(emotions)
+happy_percentage = counter['happy'] / total_sum * 100
+neutral_percentage = counter['neutral'] / total_sum * 100
+sad_percentage = counter['sad'] / total_sum * 100
+
+print("\nPercentage of happy, neutral, and sad emotions: ")
+print(f"Happy Percentage: {happy_percentage:.2f}%")
+print(f"Neutral Percentage: {neutral_percentage:.2f}%")
+print(f"Sad Percentage: {sad_percentage:.2f}%\n")
